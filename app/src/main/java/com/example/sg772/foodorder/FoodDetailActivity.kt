c8416b68453b3a90b6295fbb4fbfe7de32d79333
Nuruddin <sg7720@gmail.com>
2019-01-04 09:05:45 +0300

    'added'

---
+++ app/src/main/java/com/example/sg772/foodorder/FoodDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sg772.foodorder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_detail)
+    }
+}
